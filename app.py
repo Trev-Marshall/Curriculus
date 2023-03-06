8ad73d07e69d19c3f22599d3849203fd99d779f1
Trev-Marshall <trevor.h.marshall@icloud.com>
2023-03-06 02:26:14 -0500

    update methods on endpoint

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route("/", methods=("POST"))
+@app.route("/", methods=["POST"])
 def index():
     prompt = request.form["curriculum_prompt"]
     print(prompt)
